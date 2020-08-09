6c4192fb1108f78eb9977a415a841ab58d5da190
dstarkey23 <david.starkey@vortexa.com>
2020-08-09 11:43:52 +0100

    concatenate base model and custom output layers for transfer learning

--- TL_mobnet.py
+++ TL_mobnet.py
@@ -18,23 +18,19 @@ from keras.optimizers import Adam
 
 
 base_model=MobileNet(weights='imagenet',include_top=False) #imports the mobilenet model and discards the last 1000 neuron layer.
+base_model.trainable = False
 
-#model = keras.Sequential
+output_layers = keras.Sequential()
+output_layers.add(keras.layers.GlobalAveragePooling2D())
+output_layers.add(keras.layers.Dense(32,activation='relu'))
+#output_layers.add(keras.layers.Dense(1024,activation='relu'))
+output_layers.add(keras.layers.Dense(3,activation='softmax'))
 
-x=base_model.output
-x=GlobalAveragePooling2D()(x)
-x=Dense(1024,activation='relu')(x) #we add dense layers so that the model can learn more complex functions and classify for better results.
-x=Dense(1024,activation='relu')(x) #dense layer 2
-x=Dense(512,activation='relu')(x) #dense layer 3
-preds=Dense(3,activation='softmax')(x) #final layer with softmax activation
+final_model = keras.Sequential()
+# add resnet model
+final_model.add(base_model)
+# add the trainable output layers
+final_model.add(output_layers)
 
 
-
-model=Model(inputs=base_model.input,outputs=preds)
-
-#set the pretrained model weights untrainable.
-#Only want to optimise output layer
-for layer in model.layers[:20]:
-    layer.trainable=False
-for layer in model.layers[20:]:
-    layer.trainable=True
+final_model.summary()
