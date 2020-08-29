4f08688c15e465bbd6905e2d8f1d71fcb37aaaf3
dstarkey23 <david.starkey@vortexa.com>
2020-08-29 16:04:19 +0100

    add custom convnet model

--- ship_detection.py
+++ ship_detection.py
@@ -9,6 +9,7 @@ import pandas as pd
 import glob
 import json
 from sklearn.model_selection import train_test_split
+import keras
 
 def get_label_from_filename(data_dir = './data/images/shipsnet/shipsnet/'):
     '''
@@ -83,6 +84,49 @@ def plot_example_rgb(pic, savefile=False):
         plt.close()
 
 
+
+def define_custom_convnet():
+    '''
+    convnet with 4 sequential conv, max-pool, dropout layers
+    :return:
+    '''
+
+    # network design
+    model = keras.Sequential()
+
+    model.add(keras.layers.Conv2D(32, (3, 3), padding='same', input_shape=(80, 80, 3), activation='relu'))
+    model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))  # 40x40
+    model.add(keras.layers.Dropout(0.25))
+
+    model.add(keras.layers.Conv2D(32, (3, 3), padding='same', activation='relu'))
+    model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))  # 20x20
+    model.add(keras.layers.Dropout(0.25))
+
+    model.add(keras.layers.Conv2D(32, (3, 3), padding='same', activation='relu'))
+    model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))  # 10x10
+    model.add(keras.layers.Dropout(0.25))
+
+    model.add(keras.layers.Conv2D(32, (10, 10), padding='same', activation='relu'))
+    model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))  # 5x5
+    model.add(keras.layers.Dropout(0.25))
+
+    model.add(keras.layers.Flatten())
+    model.add(keras.layers.Dense(512, activation='relu'))
+    model.add(keras.layers.Dropout(0.5))
+
+    model.add(keras.layers.Dense(2, activation='softmax'))
+
+    # optimization setup
+    sgd = keras.optimizers.SGD(lr=0.01, momentum=0.9, nesterov=True)
+    model.compile(
+        loss='categorical_crossentropy',
+        optimizer=sgd,
+        metrics=['accuracy'])
+
+    return model
+
+
+
 if __name__ == '__main__':
 
     # download dataset from json object
@@ -95,6 +139,16 @@ if __name__ == '__main__':
     X_train, X_test, y_train, y_test = train_test_split(X, y,
                                                         test_size = 0.33,
                                                         random_state = 42)
+
+    # define the model
+    model = define_custom_convnet()
+
+    # fit the model
+    model.fit(X_train, y_train)
+
+
+
+
 
 
 
